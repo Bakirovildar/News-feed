aa4f1442df3ec6a9de7656d12c24235b0be5c92d
Ildar <bakirovildar2011@mail.ru>
2022-04-11 16:23:41 +0500

    Первый коммит приложения новости. Добавлены компоненты с v-for, открытие и закрытие новостей

---
+++ src/components/AppNews.vue
@@ -0,0 +1,53 @@
+<template>
+  <div
+      class="card"
+  >
+    <h3>{{ title }}</h3>
+    <button class="btn" @click="openNews">{{isNewOpen ? 'Закрыть' : 'Открыть'}}</button>
+    <p v-if="isNewOpen">{{ body }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  // props: ['title'],
+  props: {
+    title: String,
+    id: Number,
+    body: String,
+    isOpen: Boolean
+  },
+  data() {
+    return {
+      isNewOpen: this.isOpen
+    }
+  },
+  methods: {
+    openNews() {
+      this.isNewOpen = !this.isNewOpen
+      this.isNewOpen ? this.$emit('open-news') : ''
+    }
+  }
+}
+</script>
+
+<style>
+
+.card {
+  border: 1px solid black;
+  border-radius: 8px;
+  margin: 10px;
+  background: darkseagreen;
+}
+
+.btn {
+  border: none;
+  width: 140px;
+  height: 40px;
+  margin: 5px;
+  border-radius: 20px;
+  background: #2c3e50;
+  color: white;
+  cursor: pointer;
+}
+</style>
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,37 @@
+<template>
+  <h1>Актуальные новости {{now}}</h1>
+  <span>Открыто {{openRate}} раз</span>
+  <AppNews
+      v-for="item in news"
+      :key="item.id"
+      :title="item.title"
+      :id="item.id"
+      :body="item.body"
+      :is-open="item.isOpen"
+      @open-news="openRate++"
+  />
+</template>
+
+<script>
+import AppNews from './AppNews'
+export default {
+  data() {
+    return {
+      now: new Date().toLocaleDateString(),
+      news: [
+        {id: 1, title: 'В Уфе солнечная погода', isOpen: false, body: 'lorem ipsum Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus eaque earum, itaque libero odio quaerat. '},
+        {id: 2, title: 'Vue 3 работате отлично', isOpen: false, body: 'lorem ipsum Lorem ipsum dolor sit amet, consectetur adipisicing. '},
+      ],
+      onShowText: false,
+      openRate: 0
+    }
+  },
+  components: {
+    AppNews
+  }
+}
+
+</script>
+
+<style>
+</style>
